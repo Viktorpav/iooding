f7473861eb694d162a4935a2f558d9d3e60aeddd
Viktorpav <doncar2003@gmail.com>
2022-07-31 00:30:29 +0200

    README change 3

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -26,6 +26,22 @@ resource "aws_security_group" "allow_ssh_pub" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Django app port"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Nginx port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
