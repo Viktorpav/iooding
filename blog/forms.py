103f95c9666dcb69cd9fb4769517ed332a54f22c
Viktorpav <doncar2003@gmail.com>
2025-10-26 20:33:29 +0100

    Add Redis for pod separationtokens

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,9 @@
 from django import forms
-from .models import Post
-
+from .models import Comment
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = Post
+        model = Comment  # Changed from Post to Comment
         fields = ('name', 'email', 'body')
         widgets = {
             'body': forms.Textarea(attrs={
